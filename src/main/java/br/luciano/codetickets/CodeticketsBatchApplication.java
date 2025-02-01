f6a7e94570f25a52410c09bc53cc2a46eccb0400
Luciano Mendonca <luciano.s.mend@gmail.com>
2025-02-01 13:14:25 -0300

    inicio

---
+++ src/main/java/br/luciano/codetickets/CodeticketsBatchApplication.java
@@ -0,0 +1,13 @@
+package br.luciano.codetickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeticketsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeticketsBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/luciano/codetickets/CodeticketsBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.luciano.codetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeticketsBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
