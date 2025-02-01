7b01d67bee8ddacd5977966de0aec228d888cbdf
Luciano Mendonca <luciano.s.mend@gmail.com>
2025-02-01 15:27:15 -0300

    Criação do ItemWriter e do DataSourceConfig

--- src/main/java/br/luciano/codetickets/ImportacaoJobConfiguration.java
+++ src/main/java/br/luciano/codetickets/ImportacaoJobConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.time.LocalDate;
 
 @Configuration
 public class ImportacaoJobConfiguration {
@@ -47,6 +53,18 @@ public class ImportacaoJobConfiguration {
                 .delimited()
                 .names("cpf", "cliente", "nascimento", "evento", "data", "tipoIngresso", "valor")
                 .targetType(Importacao.class)
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<Importacao> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Importacao>()
+                .dataSource(dataSource)
+                .sql(
+                        "INSERT INTO importacao (id, cpf, cliente, evento, data tipo_ingresso, valor, hora_importacao) VALUES" +
+                                " (:id, :cpf, :cliente, :evento, :data, :tipo_ingresso, :valor, " + LocalDate.now() + " )"
+                )
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
                 .build();
     }
 }
